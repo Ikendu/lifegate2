a8f31ce49bf428e349ab89e3c698d7a04ed9cb9c
aniede <ndubest56@gmail.com>
2023-01-10 17:49:59 +0100

    module

--- app.js
+++ app.js
@@ -8,4 +8,25 @@ else
 	console.log("Large number")
 
 }
-console.log(`hey its my first node app!`)
+ console.log(`hey its my first node app!`)
+/*
+console.log(__dirname)
+console.log(require)
+console.log(__filename)
+console.log(module)
+
+console.log(process)
+
+setInterval( () => 
+{
+	console.log("Chibundu is a Software Engineer");
+}, 1000
+)
+*/
+
+const na = require('./name')
+const sayHi = require('./utils')
+
+sayHi("Chibundu")
+sayHi(na.john)
+sayHi(na.mike)
---
+++ name.js
@@ -0,0 +1,6 @@
+const secret = "SUPER SECRETE"
+const john = "John"
+const mike = "Micheal Peter"
+const look = "Lukas"
+
+module.exports = { john, mike, look }
