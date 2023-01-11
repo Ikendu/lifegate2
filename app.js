08e4472126211294ca9f02105c907aaadfe65262
aniede <ndubest56@gmail.com>
2023-01-11 18:44:41 +0100

    Asyn await

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-const http = require('http')
+const {readFile} = require('fs')
 
-const server = http.createServer( (req, res) => {
-	if (req.url === '/')
-	{
-		res.end('Welcome to our homepage')	
-	}
-	if (req.url === '/about')
-	{
-		res.end('This is who we are who we are becoming')	
-	}
-	if (req.url === '/contact')
-	{
-		res.end('This is where to meet us')	
-	}
-	res.end(`
-		<h1> Oh No! </h1>
-		<p> we cant find the page you are lookng for </p>
-		<a href="/">Back Home</a>
-		`)
-})
-server.listen(5000);
+const getText = (path) => {
+	return new Promise( (resolve, reject) => {
+		readFile(path, 'utf8', (err, res) => {
+			if (err)
+			{
+				reject(err)
+			}
+				resolve(res)
+		})
+	})
+}
+getText('./content.txt').then(
+	(res) => console.log(res))
+	.catch((err) => console.log(err))
--- http.js
+++ http.js
@@ -1,22 +1,19 @@
 const http = require('http')
 
 const server = http.createServer( (req, res) => {
-	if (req.url == '/')
-	{
-		res.end('Welcome to our homepage')	
+	if (req.url == '/'){
+		res.end('Welcome to our homepage');	
 	}
-	if (req.url == '/about')
-	{
-		res.end('This is who we are who we are becoming')	
+	if (req.url == '/about'){
+		res.end('This is who we are who we are becoming');	
 	}
-	if (req.url === '/contact')
-	{
-		res.end('This is where to meet us')	
+	if (req.url === '/contact'){
+		res.end('This is where to meet us');	
 	}
 	res.end(`
 		<h1> Oh No! </h1>
 		<p> we cant find the page you are lookng for </p>
 		<a href="/">Back Home</a>
-		`)
+		`);
 })
-server.listen(5000);
+server.listen(3000);
