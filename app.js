dce4a09b673b1979fe8d6e3f724ca00b9e182505
aniede <ndubest56@gmail.com>
2023-01-17 22:31:20 +0100

    final commit

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ var http = require('http')
 var fs = require('fs')
 
 http.createServer(function (req, res) {
-	const text = fs.readFileSync('./big.txt', 'utf8')
-	res.end(text)
-
+	//const text = fs.readFileSync('./big.txt', 'utf8')
+	//res.end(text)
+	const fileStream = fs.createReadStream('./content.txt', 'utf8');
+	fileStream.on('open', ()=>{
+		fileStream.pipe(res)
+	})
+	fileStream.on('error',(err)=>{
+		res.end(err)
+	})
 }).listen(5000)
